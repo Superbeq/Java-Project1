06fa61efd3eb1f7e7c9435fc97b520b70ee2e5e5
Beq <moalsaadi18@gmail.com>
2024-07-11 10:56:34 +0300

    The start of the project 1

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+
+
+
+        // Let's get start
+
+
+
+
+
+
+
+    } // The end of the main method
+
+
+
+
+} // The end of the main class
